0a9a915ceb5bfcd8f4ea313fcea3b467095a24f0
jolly sharma <jollykumari9168@gmail.com>
2022-02-01 18:53:08 +0530

    feat: Created wrapper component and react fragments implimentation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
@@ -15,14 +16,14 @@ const AddUser = (props) => {
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
-        message: 'Please enter a valid name and age.',
+        message: 'Please enter a valid name and age .',
       });
       return;
     }
     if (+enteredAge < 1) {
       setError({
         title: 'Invalid age',
-        message: 'Age cannot be (> 0) Please enter a valid age .',
+        message: 'Age must be (> 0)Please enter a valid age .',
       });
       return;
     }
@@ -44,7 +45,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -71,7 +72,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
